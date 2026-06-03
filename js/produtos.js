cff6904af01d0b98aa8c4ccc6fdc80161e9dd1dd
carlos34535 <carloshsl2012@hotmail.com>
2026-06-03 00:25:21 -0300

    Modify product list with new images and links
    Updated product images and affiliate links.

--- js/produtos.js
+++ js/produtos.js
@@ -11,24 +11,24 @@ const produtos = [
         preco: "R$ 39,90",
 
         // 🔥 IMAGEM (tem que ser link direto .jpg/.png)
-        imagem: "https://i.imgur.com/RAGxdGT.png",
+        imagem: "https://i.imgur.com/mQEuQJj.png",
 
         // 💰 SEU LINK DE AFILIADO
-        link: "https://shopee.com.br/seu-link-afiliado-1"
+        link: "https://s.shopee.com.br/1gFwgQRc7z"
     },
 
     {
         nome: "Smartwatch Esportivo",
         preco: "R$ 79,90",
-        imagem: "https://cf.shopee.com.br/file/efgh5678.jpg",
-        link: "https://shopee.com.br/seu-link-afiliado-2"
+        imagem: "https://i.imgur.com/wDNLJsA.png",
+        link: "https://s.shopee.com.br/4ftYG9TdmU"
     },
 
     {
         nome: "Mini Caixa de Som",
         preco: "R$ 59,90",
         imagem: "https://cf.shopee.com.br/file/ijkl9012.jpg",
-        link: "https://shopee.com.br/seu-link-afiliado-3"
+        link: "https://i.imgur.com/mSIy6Te.png"
     }
 
 ];
