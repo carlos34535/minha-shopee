70fbc81e0f1a9de8232c75a0d7887e0a8f6473d0
carlos34535 <carloshsl2012@hotmail.com>
2026-06-03 00:15:36 -0300

    Add files via upload

---
+++ js/produtos.js
@@ -0,0 +1,34 @@
+// ================================
+// LISTA DE PRODUTOS (AFILIADOS)
+// ================================
+// 👉 EDITE APENAS AQUI
+// 👉 Coloque seu link da Shopee e imagem
+
+const produtos = [
+
+    {
+        nome: "Fone Bluetooth Gamer",
+        preco: "R$ 39,90",
+
+        // 🔥 IMAGEM (tem que ser link direto .jpg/.png)
+        imagem: "https://i.imgur.com/RAGxdGT.png",
+
+        // 💰 SEU LINK DE AFILIADO
+        link: "https://shopee.com.br/seu-link-afiliado-1"
+    },
+
+    {
+        nome: "Smartwatch Esportivo",
+        preco: "R$ 79,90",
+        imagem: "https://cf.shopee.com.br/file/efgh5678.jpg",
+        link: "https://shopee.com.br/seu-link-afiliado-2"
+    },
+
+    {
+        nome: "Mini Caixa de Som",
+        preco: "R$ 59,90",
+        imagem: "https://cf.shopee.com.br/file/ijkl9012.jpg",
+        link: "https://shopee.com.br/seu-link-afiliado-3"
+    }
+
+];
